71622ce0915c6a6e747665d9f98f9a2b5b9b614e
GplCart <gplcart.software@gmail.com>
2017-07-23 20:02:32 +0300

    Update templates

--- templates/settings.php
+++ templates/settings.php
@@ -8,22 +8,18 @@
 ?>
 <form method="post" class="form-horizontal">
   <input type="hidden" name="token" value="<?php echo $_token; ?>">
-  <div class="panel panel-default">
-    <div class="panel-body">
-      <div class="form-group">
-        <label class="col-md-2 control-label"><?php echo $this->text('Key'); ?></label>
-        <div class="col-md-6">
-          <input name="settings[key]" class="form-control" value="<?php echo $this->e($settings['key']); ?>">
-          <div class="help-block"><?php echo $this->text('A key from <a href="@url">Google API Console</a>', array('@url' => 'https://developers.google.com/maps/documentation/javascript/get-api-key')); ?></div>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="col-md-4 col-md-offset-2">
-          <div class="btn-toolbar">
-            <a href="<?php echo $this->url("admin/module/list"); ?>" class="btn btn-default"><?php echo $this->text('Cancel'); ?></a>
-            <button class="btn btn-default save" name="save" value="1"><?php echo $this->text('Save'); ?></button>
-          </div>
-        </div>
+  <div class="form-group">
+    <label class="col-md-2 control-label"><?php echo $this->text('Key'); ?></label>
+    <div class="col-md-4">
+      <input name="settings[key]" class="form-control" value="<?php echo $this->e($settings['key']); ?>">
+      <div class="help-block"><?php echo $this->text('A key from <a href="@url">Google API Console</a>', array('@url' => 'https://developers.google.com/maps/documentation/javascript/get-api-key')); ?></div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-md-4 col-md-offset-2">
+      <div class="btn-toolbar">
+        <a href="<?php echo $this->url("admin/module/list"); ?>" class="btn btn-default"><?php echo $this->text('Cancel'); ?></a>
+        <button class="btn btn-default save" name="save" value="1"><?php echo $this->text('Save'); ?></button>
       </div>
     </div>
   </div>
